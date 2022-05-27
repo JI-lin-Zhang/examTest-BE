da99be3f4e905b056c16181e5010146261996a08
風雲 <9006231+rimifon@user.noreply.gitee.com>
2022-05-27 08:05:35 +0000

    !37 去掉 swagger 错误信息 * 去掉 swagger 错误信息

--- app/contract/exam.ts
+++ app/contract/exam.ts
@@ -4,7 +4,7 @@ const exam = {
 const examSubmition = {
   examineeId: { type: 'string', required: true, example: '7b6b0aec-b020-4073-973b-555134435a21' },
   tag: { type: 'string', required: true, example: 'frontend' },
-  answers: { type: 'Array[Object]', required: true, example: [{ questionId: 'a76b0aec-b020-4073-973b-555134435add', answer: 1 }] },
+  answers: { type: 'string', required: true, example: [{ questionId: 'a76b0aec-b020-4073-973b-555134435add', answer: 1 }] },
 }
 module.exports = {
   exam,
--- app/contract/question.ts
+++ app/contract/question.ts
@@ -2,7 +2,7 @@ module.exports = {
   questionPut: {
     id: { type: 'string', required: true, example: '933e6c25-557a-4255-8e6f-92d8ff76683f' },
     data: {
-      type: 'object', required: true, example: {
+      type: 'string', required: true, example: {
         title: '前端开发的基础',
         choices: [
           '前端开发的基础',
