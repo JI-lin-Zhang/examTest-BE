340e45f674cec35fb2ab2fa79d3f7babf79cde72
風雲 <9006231+rimifon@user.noreply.gitee.com>
2022-05-30 06:27:01 +0000

    !41 添加 user 的软删除 * 添加 user 的软删除

--- utils/db.ts
+++ utils/db.ts
@@ -1,7 +1,34 @@
-import { PrismaClient } from '@prisma/client'
+import { Prisma, PrismaClient } from '@prisma/client'
 
 // 引入 prismaClient
 const db = new PrismaClient()
+
+db.$use(async (params, next) => {
+  if (params.model === 'user') {
+    enum actionType {
+      delete = 'update',
+      deleteMany = 'updateMany',
+    }
+    switch (params.action) {
+      case 'delete':
+      case 'deleteMany':
+        params.action = actionType[params.action] as Prisma.PrismaAction
+        params.args.data = {
+          deletedAt: new Date(),
+          deleted: true,
+        }
+        break
+      case 'findMany':
+      case 'findFirst':
+      case 'findUnique':
+        params.args.where ??= {}
+        params.args.where.deleted = false
+        break
+      default:
+    }
+  }
+  return next(params)
+})
 
 db.$connect().catch(err => console.error(err))
 // 进行数据库的连接
